4bf376a8c3b29139722238f3a9dca8d4b398a282
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-04-10 19:37:40 +0000

    yuyen

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,61 @@
+import java.util.*;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static int fib(int num) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		return fibHelper(num, map);
+	}
+
+	public static int fibHelper(int num, HashMap<Integer, Integer> map) {
+
+		//base cases
+		if (num == 0) {
+			return 0;
+		}
+
+		if (num == 1) {
+			return 1;
+		}
+		//check the map
+		if (map.containsKey(num)) {
+			return map.get(num);
+		}
+
+		//recursive call
+		int value = fibHelper(num-1, map) + fibHelper(num-2, map);
+
+		//add to map
+		map.put(num, value);
+
+		//return
+		return value;
+	}
+
+	public static int minCostClimbingStairs(int[] cost) {
+		HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>(); 
+		int n = cost.length;
+		return Math.min(minCost(n - 1, cost, memo), minCost(n - 2, cost, memo));
+	}	
+
+	public static int minCost(int i, int[] cost, HashMap<Integer, Integer> memo) {
+		if (i < 0) {
+			return 0;
+		}
+		
+		if (i == 0 || i == 1) {
+			return cost[i];
+		}
+
+		if (memo.containsKey(i)) {
+			return memo.get(i);
+		}
+
+		int result = cost[i] + Math.min(minCost(i - 1, cost, memo), minCost(i - 2, cost, memo));
+		memo.put(i, result);
+		return result;
+	}
 }
